40f4618e0d7c90678cfb8b8f827a650d4e53c3e9
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-09-24 20:04:00 +0530

    crud added

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,6 +3,13 @@ from .import views
 
 urlpatterns = [
     path('',views.main),
-    path('home/', views.index),
-    path('home/details/<int:id>',views.details)
+    path('home/', views.index,name='index'),
+    path('home/details/<int:id>',views.details),
+    path('testing',views.testing),
+    path('home/add',views.add),
+    path('add_record',views.addrecord, name='addrecord'),
+    path('delete/<int:id>/',views.deleterecord, name='deleterecord'),
+    # path('home/update',views.update)
+    path('update/<int:id>/',views.updaterecord, name='update')
+
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
@@ -14,3 +14,34 @@ def details(request,id):
 
 def main(request):
     return render(request,'main.html')
+def testing(request):
+    context = {
+        'fruits' : ['apple','banana','cherry']
+    }
+    return render(request,'testing.html',context)
+def add(request):
+    return render(request,'add.html')
+def addrecord(request):
+    fname = request.POST['fname']
+    lname = request.POST['lname']
+    new_member = Member(firstname = fname,lastname = lname)
+    new_member.save()
+    return redirect('index')
+def deleterecord(request,id):
+    member = Member.objects.get(id=id)
+    member.delete()
+    return redirect('index')
+# def update(request):
+#     return render(request,'update.html')
+def updaterecord(request,id):
+    member = Member.objects.get(id=id)
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        
+        member.firstname = fname
+        member.lastname = lname
+        
+        member.save()
+        return redirect('index')
+    return render (request,'update.html',{'member':member})
